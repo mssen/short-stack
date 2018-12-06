4231f58945e59b6ff7fca0ca3598a031ee5c2cee
mssen <missy.danielsen@gmail.com>
2018-12-05 22:50:32 -0600

    Added Styled Components and removed layout.css in favor of a CSS reset.

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,35 @@
-import { Link } from 'gatsby';
-import PropTypes from 'prop-types';
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.div`
+  background: rebeccapurple;
+  margin-bottom: 1.45rem;
+`;
+
+const HeaderText = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+  & > h1 {
+    margin: 0;
+  }
+`;
+
+const HeaderLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+`;
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
+  <HeaderContainer>
+    <HeaderText>
+      <h1>
+        <HeaderLink to="/">{siteTitle}</HeaderLink>
       </h1>
-    </div>
-  </div>
+    </HeaderText>
+  </HeaderContainer>
 );
 
 Header.propTypes = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
 
 import Layout from '../components/layout';
 import Image from '../components/image';
+
+const MainContainer = styled.div`
+  max-width: 300px;
+  margin-bottom: 1.45rem;
+`;
 
 const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+    <MainContainer>
       <Image />
-    </div>
+    </MainContainer>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 );
