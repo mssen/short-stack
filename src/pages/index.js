670358ab583f1fde74e7f8ccfe0c7dde366d2060
mssen <missy.danielsen@gmail.com>
2018-12-13 22:06:50 -0600

    Hook up social links.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Layout from '../components/layout';
+import SocialLinks from '../components/socialLinks';
 
 const MainContainer = styled.div`
   background-size: cover;
@@ -15,6 +16,7 @@ const MainContainer = styled.div`
 const IndexPage = () => (
   <Layout>
     <MainContainer />
+    <SocialLinks />
   </Layout>
 );
 
