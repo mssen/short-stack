2c3ca8e69d7a579e9a8eb2a5423e218277930b54
mssen <missy.danielsen@gmail.com>
2019-02-01 23:45:15 -0600

    Created pages for each gallery.

---
+++ src/templates/gallery-page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+
+const GalleryPage = () => (
+  <Layout>
+    <h1>Something!</h1>
+  </Layout>
+);
+
+export default GalleryPage;
