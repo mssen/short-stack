86f2e3afa5ddbdee4297b3fbf35ce69976e069ba
mssen <missy.danielsen@gmail.com>
2019-02-01 23:03:33 -0600

    Hook into services data.

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
 
 import Layout from '../components/layout';
 
 const Services = () => (
   <Layout>
     <h1>Services</h1>
-    <p>Prices here.</p>
+    <StaticQuery
+      query={graphql`
+        query Services {
+          allContentfulService {
+            edges {
+              node {
+                title
+                price
+                description {
+                  childMarkdownRemark {
+                    html
+                  }
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={({ allContentfulService: { edges } }) =>
+        edges.map(({ node }) => (
+          <section>
+            <header>
+              <h3>{node.title}</h3>
+              <p>{node.price}</p>
+            </header>
+            <div
+              // eslint-disable-next-line react/no-danger
+              dangerouslySetInnerHTML={{
+                __html: node.description.childMarkdownRemark.html,
+              }}
+            />
+          </section>
+        ))
+      }
+    />
   </Layout>
 );
 
