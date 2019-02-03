c357750699973f88900be1f23c436ac4fc716591
mssen <missy.danielsen@gmail.com>
2019-02-03 11:50:31 -0600

    Add in simple contact form.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,7 +5,26 @@ import Layout from '../components/layout';
 const Contact = () => (
   <Layout>
     <h1>Contact</h1>
-    <p>Form here.</p>
+    <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label htmlFor="name">
+          Name: <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label htmlFor="email">
+          Email: <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <label htmlFor="message">
+          Message: <textarea name="message" />
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
   </Layout>
 );
 
