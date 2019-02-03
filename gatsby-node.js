398326471d3d244aa9de15aa40ff3b71fc6617af
mssen <missy.danielsen@gmail.com>
2019-02-03 00:06:07 -0600

    Calling Contentful for gallery links and pages.

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,28 +2,26 @@ const path = require('path');
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions;
-  return new Promise((resolve) => {
-    graphql(`
-      {
-        allContentfulGallery {
-          edges {
-            node {
-              slug
-            }
+  return graphql(`
+    {
+      allContentfulGallery {
+        edges {
+          node {
+            id
+            slug
           }
         }
       }
-    `).then((result) => {
-      result.data.allContentfulGallery.edges.forEach(({ node }) => {
-        createPage({
-          path: node.slug,
-          component: path.resolve('./src/templates/gallery-page.js'),
-          context: {
-            slug: node.slug,
-          },
-        });
+    }
+  `).then((result) => {
+    result.data.allContentfulGallery.edges.forEach(({ node }) => {
+      createPage({
+        path: `galleries/${node.slug}`,
+        component: path.resolve('./src/templates/gallery-page.js'),
+        context: {
+          galleryId: node.id,
+        },
       });
-      resolve();
     });
   });
 };
