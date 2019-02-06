1493526719603316d1c8915401fb2f805ddc573c
mssen <missy.danielsen@gmail.com>
2019-02-05 21:58:34 -0600

    Remove picture.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import styled from 'styled-components';
 
 import Layout from '../components/layout';
 
-const MainContainer = styled.div`
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  height: 100vh;
-  width: 100vw;
-  background-image: url(https://source.unsplash.com/a8pXN9_5R2M/1600x900);
-`;
-
 const IndexPage = () => (
   <Layout>
-    <MainContainer />
+    <p>Stuff here</p>
   </Layout>
 );
 
