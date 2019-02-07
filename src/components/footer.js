cd4d04343574b6e88caa6c4b299ac81b6da78fb8
mssen <missy.danielsen@gmail.com>
2019-02-06 22:05:28 -0600

    Style footer.

---
+++ src/components/footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import SocialLinks from './socialLinks';
+import { th } from '../style/theme';
+
+const FooterContainer = styled.footer`
+  background: ${th('grayLight')};
+  height: 75px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  & > * {
+    margin-right: 1rem;
+  }
+`;
+
+const Footer = () => (
+  <FooterContainer>
+    <SocialLinks />
+  </FooterContainer>
+);
+
+export default Footer;
--- src/components/socialLinks.js
+++ src/components/socialLinks.js
@@ -1,54 +1,66 @@
 import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
+import styled from 'styled-components';
 
-import facebookIcon from '../images/facebook.svg';
-import instagramIcon from '../images/instagram.svg';
-import emailIcon from '../images/email.svg';
+import FacebookIcon from '../images/facebook.svg';
+import InstagramIcon from '../images/instagram.svg';
+import EmailIcon from '../images/email.svg';
+import { th } from '../style/theme';
+
+const IconLink = styled.a`
+  & > svg {
+    fill: ${th('main')};
+    transition: fill 200ms ease-in-out, transform 100ms ease;
+  }
+
+  & > svg:hover,
+  & > svg:focus {
+    fill: ${th('mainDark')};
+  }
+`;
 
 const SocialLinks = () => (
-  <footer>
-    <StaticQuery
-      query={graphql`
-        query SocialLinks {
-          allContentfulSocialLink {
-            edges {
-              node {
-                type
-                url
-                email
-              }
+  <StaticQuery
+    query={graphql`
+      query SocialLinks {
+        allContentfulSocialLink {
+          edges {
+            node {
+              type
+              url
+              email
             }
           }
         }
-      `}
-      render={({ allContentfulSocialLink: { edges } }) =>
-        edges.map(({ node }) => {
-          switch (node.type) {
-            case 'Facebook':
-              return (
-                <a href={node.url}>
-                  <img src={facebookIcon} alt="Facebook Link" />
-                </a>
-              );
-            case 'Instagram':
-              return (
-                <a href={node.url}>
-                  <img src={instagramIcon} alt="Instagram Link" />
-                </a>
-              );
-            case 'Email':
-              return (
-                <a href={node.email}>
-                  <img src={emailIcon} alt="Email Link" />
-                </a>
-              );
-            default:
-              return null;
-          }
-        })
       }
-    />
-  </footer>
+    `}
+    render={({ allContentfulSocialLink: { edges } }) =>
+      edges.map(({ node }) => {
+        switch (node.type) {
+          case 'Facebook':
+            return (
+              <IconLink href={node.url} alt="Facebook Link">
+                <FacebookIcon />
+              </IconLink>
+            );
+          case 'Instagram':
+            return (
+              <IconLink href={node.url} alt="Instagram Link">
+                <InstagramIcon />
+              </IconLink>
+            );
+          case 'Email':
+            return (
+              <IconLink href={node.email} alt="Email Link">
+                <EmailIcon />
+              </IconLink>
+            );
+          default:
+            return null;
+        }
+      })
+    }
+  />
 );
 
 export default SocialLinks;
