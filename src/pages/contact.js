d62325b37702dfdb2980a12a5fc4431a1710c2b5
mssen <missy.danielsen@gmail.com>
2019-02-07 20:46:40 -0600

    Hook up contact blurb with Contentful.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
 
 import Layout from '../components/layout';
 import Button from '../style/button';
@@ -6,6 +7,30 @@ import Button from '../style/button';
 const Contact = () => (
   <Layout>
     <h1>Contact</h1>
+    <StaticQuery
+      query={graphql`
+        {
+          contentfulPage(pageTitle: { eq: "Contact" }) {
+            text {
+              childContentfulRichText {
+                html
+              }
+            }
+          }
+        }
+      `}
+      render={({ contentfulPage }) => (
+        <React.Fragment>
+          <h2>Let&apos;s get in touch!</h2>
+          <article
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: contentfulPage.text.childContentfulRichText.html,
+            }}
+          />
+        </React.Fragment>
+      )}
+    />
     <form name="contact" method="POST" data-netlify="true">
       <p>
         <label htmlFor="name">
