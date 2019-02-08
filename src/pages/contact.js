31fbde3929061ac42d25031f9d2c514407283da0
mssen <missy.danielsen@gmail.com>
2019-02-07 21:52:05 -0600

    Simple grid for contact.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,57 +1,74 @@
 import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
+import styled from 'styled-components';
 
 import Layout from '../components/layout';
 import Button from '../style/button';
 import PageHeader from '../style/pageHeader';
 
+const ContactContainer = styled.section`
+  display: grid;
+  grid-gap: 1rem;
+  grid-template-columns: 1fr;
+
+  @media (min-width: 700px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`;
+
 const Contact = () => (
   <Layout>
     <PageHeader>Contact</PageHeader>
-    <StaticQuery
-      query={graphql`
-        {
-          contentfulPage(pageTitle: { eq: "Contact" }) {
-            text {
-              childContentfulRichText {
-                html
+    <ContactContainer>
+      <div>
+        <StaticQuery
+          query={graphql`
+            {
+              contentfulPage(pageTitle: { eq: "Contact" }) {
+                text {
+                  childContentfulRichText {
+                    html
+                  }
+                }
               }
             }
-          }
-        }
-      `}
-      render={({ contentfulPage }) => (
-        <React.Fragment>
-          <h3>Let&apos;s get in touch!</h3>
-          <article
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{
-              __html: contentfulPage.text.childContentfulRichText.html,
-            }}
-          />
-        </React.Fragment>
-      )}
-    />
-    <form name="contact" method="POST" data-netlify="true">
-      <p>
-        <label htmlFor="name">
-          Name: <input type="text" name="name" />
-        </label>
-      </p>
-      <p>
-        <label htmlFor="email">
-          Email: <input type="email" name="email" />
-        </label>
-      </p>
-      <p>
-        <label htmlFor="message">
-          Message: <textarea name="message" />
-        </label>
-      </p>
-      <p>
-        <Button type="submit">Send</Button>
-      </p>
-    </form>
+          `}
+          render={({ contentfulPage }) => (
+            <React.Fragment>
+              <h3>Let&apos;s get in touch!</h3>
+              <article
+                // eslint-disable-next-line react/no-danger
+                dangerouslySetInnerHTML={{
+                  __html: contentfulPage.text.childContentfulRichText.html,
+                }}
+              />
+            </React.Fragment>
+          )}
+        />
+      </div>
+      <div>
+        <form name="contact" method="POST" data-netlify="true">
+          <p>
+            <label htmlFor="name">
+              Name: <input type="text" name="name" />
+            </label>
+          </p>
+          <p>
+            <label htmlFor="email">
+              Email: <input type="email" name="email" />
+            </label>
+          </p>
+          <p>
+            <label htmlFor="message">
+              Message: <textarea name="message" />
+            </label>
+          </p>
+          <p>
+            <Button type="submit">Send</Button>
+          </p>
+        </form>
+      </div>
+    </ContactContainer>
   </Layout>
 );
 
