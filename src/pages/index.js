9caf669b94d4d44ef9b0bdef4f35f2c4bf030307
mssen <missy.danielsen@gmail.com>
2019-04-18 22:23:06 -0500

    Add core-js for Array.fill and fix security vulnerabilities

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StaticQuery, graphql, Link } from 'gatsby';
 import styled from 'styled-components';
+import 'core-js/features/array/fill';
 
 import Layout from '../components/layout';
 import Testimonials from '../components/testimonials';
