f019cac7fdba36ef31606d835c28904c35f185eb
mssen <missy.danielsen@gmail.com>
2019-05-02 21:23:37 -0500

    Add window check to prevent build issues

--- src/hooks/useMedia.js
+++ src/hooks/useMedia.js
@@ -10,20 +10,24 @@ import { useEffect, useState } from 'react';
  */
 
 export default function useMedia(queries, values, defaultValue) {
-  const mediaQueryLists = queries.map((q) => window.matchMedia(q));
+  // Need to check for window to prevent build issues
+  if (typeof window !== 'undefined' && window) {
+    const mediaQueryLists = queries.map((q) => window.matchMedia(q));
 
-  const getValue = () => {
-    const index = mediaQueryLists.findIndex((mql) => mql.matches);
-    return typeof values[index] !== 'undefined' ? values[index] : defaultValue;
-  };
+    const getValue = () => {
+      const index = mediaQueryLists.findIndex((mql) => mql.matches);
+      return typeof values[index] !== 'undefined' ? values[index] : defaultValue;
+    };
 
-  const [value, setValue] = useState(getValue);
+    const [value, setValue] = useState(getValue);
 
-  useEffect(() => {
-    const handler = () => setValue(getValue);
-    mediaQueryLists.forEach((mql) => mql.addListener(handler));
-    return () => mediaQueryLists.forEach((mql) => mql.removeListener(handler));
-  }, []);
+    useEffect(() => {
+      const handler = () => setValue(getValue);
+      mediaQueryLists.forEach((mql) => mql.addListener(handler));
+      return () => mediaQueryLists.forEach((mql) => mql.removeListener(handler));
+    }, []);
+    return value;
+  }
 
-  return value;
+  return defaultValue;
 }
